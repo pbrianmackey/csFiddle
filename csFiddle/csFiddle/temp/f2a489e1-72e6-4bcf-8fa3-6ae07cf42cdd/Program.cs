f723ced7b936456c14cbaf50cbb1944cdf2020ca
Brian Mackey <mackey.brian@gmail.com>
2013-03-27 15:47:32 -0500

    Starting to plug the compiler into the UI

---
+++ csFiddle/csFiddle/temp/f2a489e1-72e6-4bcf-8fa3-6ae07cf42cdd/Program.cs
@@ -0,0 +1,14 @@
+Console.WriteLine("Hello Chuck!");Console.ReadLine();using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //$$$BeginCode
+        }
+    }
+}
