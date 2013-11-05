1a1628a1e62c45301014d7e5e879fad9904c2ff8
unknown <e78590@IDSPBM01.bhcs.pvt>
2013-11-05 14:48:35 -0600

    fixed bugs to allow remote deploy

--- csFiddle/csFiddle/Controllers/CodeProcessorController.cs
+++ csFiddle/csFiddle/Controllers/CodeProcessorController.cs
@@ -11,6 +11,7 @@ using Roslyn.Services.Host;
 using Roslyn.Compilers;
 using System.Diagnostics;
 using csFiddle.BL;
+using System.Web;
 
 namespace csFiddle.Controllers
 {
@@ -25,78 +26,60 @@ namespace csFiddle.Controllers
 
         private static string CompileWithRoslyn(string codeToProcess)
         {
-            const string fileName = "MyCompilation3";
-            string outputFileName = Path.Combine(Path.GetTempPath(), fileName + ".exe");
-            string pdbFileName = Path.Combine(Path.GetTempPath(), fileName + ".pdb");
-            var tree = SyntaxTree.ParseText(codeToProcess);
-
-            MetadataReference mscorlib = MetadataReference.CreateAssemblyReference("mscorlib");
-
-            var comp = Compilation.Create(
-                 fileName,
-                 syntaxTrees: new[] { tree },
-                 references: new[] { mscorlib });
-
-            EmitResult result = null;
-
-            using (var ilStream = new FileStream(outputFileName, FileMode.OpenOrCreate))
-            using (var pdbStream = new FileStream(pdbFileName, FileMode.OpenOrCreate))
-            {
-                // Emit IL for the compiled program.
-                result = comp.Emit(
-                    outputStream: ilStream,
-                    pdbFileName: pdbFileName,
-                    pdbStream: pdbStream);
-            }
-
             var output = "";
-            if (result.Success)
+            try
             {
-                // Run the compiled program.
-                output = ProcessHelper.Start(outputFileName);
-            }
-            else
-            {
-                foreach (var diag in result.Diagnostics)
+                output = "test";
+                string webRoot = HttpContext.Current.Server.MapPath("~/");
+                string tempDirectory = Path.Combine(webRoot, "TempProgs");
+                //output = tempDirectory;//Environment.CurrentDirectory returns: C:\Windows\SysWOW64\inetsrv\TempProgs
+                const string fileName = "MyCompilation3";
+                string outputFileName = Path.Combine(Path.GetTempPath(), fileName + ".exe");
+                string pdbFileName = Path.Combine(Path.GetTempPath(), fileName + ".pdb");
+                output = codeToProcess;
+                //var aType = Roslyn.Compilers.CSharp.TypeKind.Class;//Test call to load a roslyn dll
+                var tree = SyntaxTree.ParseText(codeToProcess);
+
+                MetadataReference mscorlib = MetadataReference.CreateAssemblyReference("mscorlib");
+
+                var comp = Compilation.Create(
+                    fileName,
+                    syntaxTrees: new[] { tree },
+                    references: new[] { mscorlib });
+
+                EmitResult result = null;
+
+                using (var ilStream = new FileStream(outputFileName, FileMode.OpenOrCreate))
+                using (var pdbStream = new FileStream(pdbFileName, FileMode.OpenOrCreate))
                 {
-                    //Console.WriteLine(diag.ToString());
-                    output += diag;//Assume few concatenations, skipping out on StringBuilder
+                    // Emit IL for the compiled program.
+                    result = comp.Emit(
+                        outputStream: ilStream,
+                        pdbFileName: pdbFileName,
+                        pdbStream: pdbStream);
                 }
+
+
+                if (result.Success)
+                {
+                    // Run the compiled program.
+                    output = ProcessHelper.Start(outputFileName);
+                }
+                else
+                {
+                    foreach (var diag in result.Diagnostics)
+                    {
+                        //Console.WriteLine(diag.ToString());
+                        output += diag; //Assume few concatenations, skipping out on StringBuilder
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
             }
 
             return output;
-
-            //ProjectId pid1;
-            //DocumentId did1;
-            //ISolution solution = Solution.Create(SolutionId.CreateNewId("Solution"))
-            //                            .AddCSharpProject("Project1.dll", "Project1", out pid1)
-            //                            .AddDocument(pid1, "A.cs", codeToProcess, out did1);
-
-            //var workspaceServices = (IHaveWorkspaceServices)solution;
-            //var projectDependencyService = workspaceServices.WorkspaceServices.GetService<IProjectDependencyService>();
-            //var assemblies = new List<MemoryStream>();
-            //foreach (var projectId in projectDependencyService.GetDependencyGraph(solution).GetTopologicallySortedProjects())
-            //{
-            //    var stream = new MemoryStream();
-            //    {
-            //        solution.GetProject(projectId).GetCompilation().Emit(stream);
-            //        assemblies.Add(stream);
-            //    }
-            //    stream.Position = 0;
-            //}
-
-            ////Check whats in the stream
-            //foreach (var s in assemblies)
-            //{
-            //    var resultOuter = System.Text.Encoding.ASCII.GetString(s.ToArray());//s is empty string
-            //    using (var sr = new StreamReader(s))
-            //    {
-            //        string result = sr.ReadToEnd();
-
-            //        System.Diagnostics.Trace.Write(result);
-            //        s.Close();
-            //    }
-            //}
         }
 
         private void CompileWithVisualStudioInstallation(string codeToProcess)
---
+++ csFiddle/csFiddle/obj/Release/Package/PackageTmp/Readme.txt
@@ -0,0 +1,2 @@
+﻿Consider implementing Roslyn when it's released: http://msdn.microsoft.com/en-us/vstudio/roslyn.aspx
+
